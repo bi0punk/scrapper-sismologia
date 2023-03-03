0cb90d867631ae171d9adb768ab3c2f468a6c207
sysbot <systecinfo_cl@criptext.com>
2023-03-03 00:36:31 -0300

    guardando data

--- maindata.py
+++ maindata.py
@@ -8,37 +8,36 @@ import pandas as pd
 
 while True:
     for i in range(1,31):
-        print(i)
+        print("\nGenerando direccion . . . ")
+        time.sleep(1)
         
         if i >= 10:
             link_base = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/202001{i}.html'
             
         else:
             link_base = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/2020010{i}.html'
-            
-        
 
-        print("Generando direccion . . . \n")
-
+        print(f'La direccion generada es {link_base} ')
         response = requests.get(link_base)
         code = (response.status_code)
-
+        print(f'Codigo de respuesta: {code}')
+        time.sleep(1)
         if code != 200:
             print("Direccion no disponible esperando . . .")
             time.sleep(60)
-
+        print("Obteniendo informacion . . .")
         table_MN = pd.read_html(link_base)
-        print(f'Total tables: {len(table_MN)}')
+        #print(f'Total tables: {len(table_MN)}')
 
-
-        global df
         df = table_MN[1]
         
-        print(df)
+        info = (df.head())
+        print(info)
         """ df.to_csv('file_name.csv', encoding='utf-8') """
-        print("Escribiendo data en csv . . .")
-        df.to_csv('datasismos.csv', mode='a', index=False, header=True, encoding='utf-8')
+        print("\nEscribiendo data en csv . . .")
+        df.to_csv('datasismos.csv', mode='a', index=False, header=False, encoding='utf-8')
         fecha_data = datetime.datetime.now()
+        time.sleep(1)
         print("\n")
 
     time.sleep(40) 
