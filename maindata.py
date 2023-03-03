601f4f3a7ca7ed4dafa6ff4f405f24733efbb3a1
sysbot <systecinfo_cl@criptext.com>
2023-03-02 23:52:08 -0300

    add status code

--- maindata.py
+++ maindata.py
@@ -1,26 +1,37 @@
-
-import time
-import pyttsx3  
 import threading
+import datetime
+import pyttsx3  
+import time
+import pandas as pd
 
 
-def timer():
-    while True:
-        day = 0 
-        year = None
-        month = None
-        link = f'Este es el enlace, el dia a consultar es {day}'
-        print(link)
-        dia = 0    
-        semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
-
-        #entra a cilco while interno y no se detenie hasta que se cumpla o asigne una condicion dada
-        while dia < 7:
-            print("Hoy es " + semana[dia])
-            dia = dia + 1
-            time.sleep(2)
-            """ time.sleep(600)  """  # n segundos.
+while True:
+    for i in range(1,31):
+        print(i)
         
-# Iniciar la ejecución en segundo plano.
-t = threading.Thread(target=timer)
-t.start()
+        if i >= 10:
+            link_base = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/202001{i}.html'
+            
+        else:
+            link_base = f'https://www.sismologia.cl/sismicidad/catalogo/2020/01/2020010{i}.html'
+            
+        
+
+        print("Generando direccion . . . /n")
+
+        table_MN = pd.read_html(link_base)
+        print(f'Total tables: {len(table_MN)}')
+
+        global df
+        df = table_MN[1]
+        
+        print(df)
+        """ df.to_csv('file_name.csv', encoding='utf-8') """
+        print("Escribiendo data en csv")
+        df.to_csv('datasismos.csv', mode='a', index=False, header=True, encoding='utf-8')
+        fecha_data = datetime.datetime.now()
+        print("\n")
+
+    time.sleep(40) 
+
+
